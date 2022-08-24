837f57fc9a7ed57389b027dbe1c1a6468aba3e97
Rafael Araújo Oliveira Gomes <rafaelufjf@gmail.com>
2022-08-24 14:08:26 -0300

    github actions

--- django_app/settings.py
+++ django_app/settings.py
@@ -31,7 +31,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+#DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+DEVELOPMENT_MODE = "True"
 
 # Application definition
 
