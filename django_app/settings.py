232314ff57811c1ce56187d83be88b61a8a822b2
Rafael Araújo Oliveira Gomes <rafaelufjf@gmail.com>
2022-08-27 08:32:05 -0300

    arquivos estáticos

--- django_app/settings.py
+++ django_app/settings.py
@@ -139,18 +139,27 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_URL = "/static/"
+
+# Caso seja necessário carregar um arquivo css global não atrelado a um app.
+STATICFILES_DIRS = [
+    BASE_DIR / "base_static",
+]
+
+# Pasta criado apos python manage.py collectstatic
+# Importante criar sempre namespace para evitar colisão de nomes
+# evitar nomes iguais -- global-style.css - style.css
+# por isso reviews - templates - reviews de novo
+
+STATIC_ROOT = BASE_DIR / "static"
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-
 # Django Debug Toolbar
 
 INTERNAL_IPS = [
