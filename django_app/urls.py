ddae5601e982d7f25e1b8349c3153aa3d82c7d6a
Rafael Araújo Oliveira Gomes <rafaelufjf@gmail.com>
2022-08-28 09:00:07 -0300

    urls dinâmicas

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-
-
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('books.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("books.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
