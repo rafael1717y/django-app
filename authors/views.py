b916bbca7e0ffc3d9fba3b9e8c33fb2addbd5140
Rafael Araújo Oliveira Gomes <rafaelufjf@gmail.com>
2022-09-01 08:36:25 -0300

    início criação do form para registro de usuário

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.http import Http404
 
 
 def register_view(request):
-    return render(request, "authors/pages/register_view.html")
+    """Redireciona para essa view após usuário criado."""
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)    
+    return render(request, "authors/pages/register_view.html", {
+        'form': form,
+    })
+
+def register_create(request):
+    """Criação de um registro"""
+    if not request.POST:
+        raise Http404()
+    
+    POST = request.POST 
+    request.session['register_form_data'] = POST 
+    form = RegisterForm(POST)    
+
+    return redirect('authors:register')
