6cec3bddbf1e79690138231d240cd6c78828aa64
Rafael Araújo Oliveira Gomes <rafaelufjf@gmail.com>
2022-09-02 10:09:49 -0300

    formulário de login

---
+++ authors/forms/__init__.py
@@ -0,0 +1,2 @@
+from .login import LoginForm
+from .register_form import RegisterForm
---
+++ authors/forms/login.py
@@ -0,0 +1,14 @@
+from django import forms
+from utils.django_forms import add_placeholder
+
+
+class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Digite seu nome de usuário')
+        add_placeholder(self.fields['password'], 'Digite a sua senha')
+
+    username = forms.CharField()
+    password = forms.CharField(
+        widget=forms.PasswordInput()
+    )
