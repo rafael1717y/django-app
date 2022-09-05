0863e345f91433ead11b9abb448d13536a652ef4
Rafael Gomes <rafaelufjf@gmail.com>
2022-09-05 17:02:28 -0300

    Update test_pagination.py

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -6,7 +6,7 @@ class PaginationTest(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
-            qty_pagubas=4,
+            qty_paginas=4,
             current_page=1,
         )
         self.assertEqual([1, 2, 3, 4], pagination)
