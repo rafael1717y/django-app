a58ba3410044f068194e4a9d3229b7429456e603
Rafael Araújo Oliveira Gomes <rafaelufjf@gmail.com>
2022-09-06 11:24:08 -0300

    correção paginação

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,11 +2,6 @@ import math
 
 from django.core.paginator import Paginator
 
-# python -c
-# "import string as s;from random import SystemRandom as
-# sr;print(''.join(sr().choices(s.ascii_letters +
-# s.punctuation, k=64)))"
-
 
 def make_pagination_range(
     page_range,
@@ -29,21 +24,21 @@ def make_pagination_range(
 
     pagination = page_range[start_range:stop_range]
     return {
-        "pagination": pagination,
-        "page_range": page_range,
-        "qty_pages": qty_pages,
-        "current_page": current_page,
-        "total_pages": total_pages,
-        "start_range": start_range,
-        "stop_range": stop_range,
-        "first_page_out_of_range": current_page > middle_range,
-        "last_page_out_of_range": stop_range < total_pages,
+        'pagination': pagination,
+        'page_range': page_range,
+        'qty_pages': qty_pages,
+        'current_page': current_page,
+        'total_pages': total_pages,
+        'start_range': start_range,
+        'stop_range': stop_range,
+        'first_page_out_of_range': current_page > middle_range,
+        'last_page_out_of_range': stop_range < total_pages,
     }
 
 
 def make_pagination(request, queryset, per_page, qty_pages=4):
     try:
-        current_page = int(request.GET.get("page", 1))
+        current_page = int(request.GET.get('page', 1))
     except ValueError:
         current_page = 1
 
@@ -51,7 +46,9 @@ def make_pagination(request, queryset, per_page, qty_pages=4):
     page_obj = paginator.get_page(current_page)
 
     pagination_range = make_pagination_range(
-        paginator.page_range, qty_pages, current_page
+        paginator.page_range,
+        qty_pages,
+        current_page
     )
 
     return page_obj, pagination_range
